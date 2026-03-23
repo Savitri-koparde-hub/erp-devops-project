c32648fed0bfd2ffe78226f0af6fd184ae81c6a7
Savitri Koparde <savitrikoparde612@gmail.com>
2026-03-23 23:43:59 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import Toast from 'react-native-toast-message';
+import { AuthProvider } from './src/contexts/AuthContext';
+import { ThemeProvider } from './src/contexts/ThemeContext';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <NavigationContainer>
+          <StatusBar style="auto" />
+          <AppNavigator />
+          <Toast />
+        </NavigationContainer>
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
